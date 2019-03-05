2cd7a71885855ff9d05d17487f2b114c740c61ef
Morgan29200 <40162816+Morgan29200@users.noreply.github.com>
2019-03-05 17:23:59 +0100

    Add liquid capsule
    Capsule de liquide qui bouge comme IRL

---
+++ Assets/script/Movement_Glass.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using SimpleJSON;
+
+public class Movement_Glass : MonoBehaviour
+{
+
+    public Vector3 pointB;
+    public Vector3 pointA;
+    public float maxSpeed = 1;
+    public bool pressHit = false;
+    public bool pressHit2 = false;
+
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.L))
+        {
+            pressHit = true;
+
+        }
+
+
+        if (pressHit == true)
+        {
+            pressHit2 = false;
+            var change = maxSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, pointB, change);
+
+        }
+
+
+        if (Input.GetKey(KeyCode.M))
+        {
+            pressHit2 = true;
+        }
+
+        if (pressHit2 == true)
+        {
+            pressHit = false;
+            var change2 = maxSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, pointA, change2);
+        }
+
+    }
+
+}
